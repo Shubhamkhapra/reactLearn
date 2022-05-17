b000ea62d0297c126ca1e5e1bf3e7f8e7590eef6
sk <shubhamkhapra21@gmail.com>
2022-05-18 00:10:33 +0530

    Updates

---
+++ precache-manifest.9945160c014023dd33ec743fa167cc02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16d34171cbdfd7cd31d1",
+    "url": "/reactLearn/static/css/main.6ee80f8c.chunk.css"
+  },
+  {
+    "revision": "16d34171cbdfd7cd31d1",
+    "url": "/reactLearn/static/js/main.16d34171.chunk.js"
+  },
+  {
+    "revision": "2117a9af24bc9b9ffc07",
+    "url": "/reactLearn/static/css/1.e642f6ea.chunk.css"
+  },
+  {
+    "revision": "2117a9af24bc9b9ffc07",
+    "url": "/reactLearn/static/js/1.2117a9af.chunk.js"
+  },
+  {
+    "revision": "12186c4958839ba39137",
+    "url": "/reactLearn/static/js/runtime~main.12186c49.js"
+  },
+  {
+    "revision": "31191297dae5774025be0218d81f8e2c",
+    "url": "/reactLearn/index.html"
+  }
+];
